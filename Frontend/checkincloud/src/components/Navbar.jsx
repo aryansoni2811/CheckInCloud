c3892b6eeb92a04ebfb65ea840ec821af339f07b
aryansoni2811 <145922291+aryansoni2811@users.noreply.github.com>
2025-02-09 01:10:42 +0530

    Update Navbar.jsx

--- Frontend/checkincloud/src/components/Navbar.jsx
+++ Frontend/checkincloud/src/components/Navbar.jsx
@@ -1,39 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Phegon Hotel
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar">
+      <div className="nav-brand">
+        <Link to="/">Check In Cloud</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
       </div>
     </nav>
   );
